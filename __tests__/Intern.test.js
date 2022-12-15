186fc14cffa5ffae1feb2a0c002f883c9008a8b3
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-12-14 23:44:50 -0500

    Added other classes that pass the tests
    fixed some mistakes in Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,12 @@
 const Intern = require("../lib/Intern");
 const intern = new Intern("name", "id", "email", "school");
 describe("Intern", () => {
-    it("should have github username property", () => {
+    it("should have school name property", () => {
         expect(intern).toHaveProperty("school", "school");
     });
-    it("should have getGithub() method and it should return github username", () => {
-        expect(intern.getGithub()).toBeDefined();
-        expect(intern.getGithub()).toBe("@github");
+    it("should have getSchool() method and it should return school username", () => {
+        expect(intern.getSchool()).toBeDefined();
+        expect(intern.getSchool()).toBe("school");
     });
     it("should have getRole() method and it should return role (Intern)", () => {
         expect(intern.getRole()).toBeDefined();
